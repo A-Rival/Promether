84a75185b63e9788b63b8a3bb99495544ab27567
HappyTanuki <rlaalstn120@icloud.com>
2023-09-25 23:28:35 +0900

    develop 파일 복구

---
+++ Source/Promether/AnimNotify/AN_AnimEnd.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "AN_AnimEnd.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROMETHER_API UAN_AnimEnd : public UAnimNotify
+{
+	GENERATED_BODY()
+
+public:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};

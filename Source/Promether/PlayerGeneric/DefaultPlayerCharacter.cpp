9d07b34c064c86273584c08d91f95a9ff3b3746b
PeterPan <lkjhg1642@gmail.com>
2023-10-21 16:08:53 +0900

    복원

--- Source/Promether/PlayerGeneric/DefaultPlayerCharacter.cpp
+++ Source/Promether/PlayerGeneric/DefaultPlayerCharacter.cpp
@@ -16,7 +16,7 @@ ADefaultPlayerCharacter::ADefaultPlayerCharacter()
 	CameraSpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	CameraSpringArm->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, 0.0f), FRotator(-45.0f, 0.0f, 0.0f));
 	CameraSpringArm->SetAbsolute(false, true, true);
-	CameraSpringArm->TargetArmLength = 1000.f;
+	CameraSpringArm->TargetArmLength = 2000.f;
 	CameraSpringArm->bEnableCameraLag = true;
 	CameraSpringArm->bDoCollisionTest = false;
 	CameraSpringArm->CameraLagSpeed = 3.0f;

fbc5e4d30ef42e0905848c73081b161b09918f94
HappyTanuki <rlaalstn120@icloud.com>
2023-10-27 22:54:37 +0900

    저장

--- Source/Promether/Promether.Build.cs
+++ Source/Promether/Promether.Build.cs
@@ -8,7 +8,20 @@ public class Promether : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "NavigationSystem", "OnlineSubsystem" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+			"Engine",
+			"InputCore",
+			"EnhancedInput",
+			"AIModule",
+			"NavigationSystem",
+			"OnlineSubsystem",
+			"GameplayAbilities",
+			"GameplayTags",
+			"GameplayTasks"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
